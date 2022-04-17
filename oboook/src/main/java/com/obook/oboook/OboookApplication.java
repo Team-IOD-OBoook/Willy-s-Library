88bc534db0310a88353fb3467c337d29ced99e07
sehun <owstork94@gmail.com>
2022-04-17 23:06:53 +0900

    intitalize SpringProject

---
+++ oboook/src/main/java/com/obook/oboook/OboookApplication.java
@@ -0,0 +1,13 @@
+package com.obook.oboook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OboookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OboookApplication.class, args);
+	}
+
+}
---
+++ oboook/src/test/java/com/obook/oboook/OboookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obook.oboook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OboookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
